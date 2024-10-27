e56ed8def90061148303720da79ae4090b6323a4
DHANUSH <DHANUSH@LAPTOP-6KI0CVPR>
2024-10-27 20:01:07 +0530

    day2 commit

--- C2TC_DHANUSH/src/com/tns/ifet/day2/DoWhileDemo.java
+++ C2TC_DHANUSH/src/com/tns/ifet/day2/DoWhileDemo.java
@@ -1,11 +1,12 @@
 package com.tns.ifet.day2;
 
-public class BreakDemo {
+public class DoWhileDemo {
 	public static void main(String[] args) {
-		for(int i=1;i<10;i++){
-		if(i==5)
-		break;
+		int i=-7;
+		do{
 		System.out.println(i);
+		i++;
+		} 
+		while(i<=7);
 		}
-	}
 }
